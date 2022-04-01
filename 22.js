1ab8d6a79ed7316fbed967eea77676a1a9266cff
AsyaCht <anastasiya.chubata@gmail.com>
2022-04-01 20:21:09 +0300

    01

---
+++ 22.js
@@ -0,0 +1,5 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+const wordBlanks = "My "+ myNoun + " very " + myAdjective + " and he " + myVerb + " " + myAdverb + "." ;
