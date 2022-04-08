46896e628e9ded40d16459eb119d40808606596a
AsyaCht <anastasiya.chubata@gmail.com>
2022-04-08 13:52:16 +0300

    01

---
+++ 42.js
@@ -0,0 +1,8 @@
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift();
+  
+  }
+  const testArr = [1, 2, 3, 4, 5];
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
